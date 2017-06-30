f05dfe5bd78a1c8f4a21901d8e0be2982841b06e
Joshua Birch <snowpigdog@gmail.com>
2017-06-30 15:33:29 -0600

    learning how git commits down the directory

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc(temp)
+	cels = (temp.to_f - 32) * 5/9
+	return cels
+end
+
+def ctof(temp)
+	fahr = (temp.to_f * 9/5) + 32
+	return fahr
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,40 @@
 #write your code here
+def add(var1, var2)
+	ans = var1 + var2
+	return ans
+end
+
+def subtract (var1, var2)
+	ans = var1 - var2
+	return ans
+end
+
+def sum(array)
+	ans = 0
+	array.each do |i|
+		ans += i
+	end
+	return ans
+end
+
+def multiply(var1, var2)
+	ans = var1 * var2
+	return ans
+end
+
+def power(base, exp)
+	ans = base**exp
+	return ans
+end
+
+def factorial(num)
+	if num == 0
+		return 1
+	elsif num == 1
+		return 1
+	else
+		ans = num * factorial(num - 1)
+		return ans
+	end
+end
+
