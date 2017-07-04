ffef94bfeee7fffb25db7c53af8fad40b6f13a8c
Joshua Birch <snowpigdog@gmail.com>
2017-07-04 07:47:51 -0600

    last commit

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,33 @@
+
+
+
 class Timer
-  #write your code here
+
+	def initialize
+		@seconds = 0
+	end
+
+	def padded(int)
+		int = int.to_s
+		if int.length == 1
+			return "0" + int
+		else
+			return int
+		end
+	end
+
+	def seconds
+		@seconds
+	end
+
+	def seconds=(var)
+		@seconds = var
+	end
+
+	def time_string
+		time_string = padded(seconds / 3600)+ ':' + padded((seconds%3600)/60) + ':' + padded(seconds%60)
+	end
+	
 end
+
+
