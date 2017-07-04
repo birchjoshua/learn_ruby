c04b6bc76eb7af1182b10f3034a51b50f29e1c29
Joshua Birch <snowpigdog@gmail.com>
2017-07-03 22:57:36 -0600

    Books is done

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,25 @@
 class Book
-# write your code here
+
+	def title
+		@title
+	end
+
+	def title=(value)
+		bads = ['and', 'for', 'nor', 'but', 'or', 'yet',\
+		'so', 'with', 'at', 'from', 'into', 'the', 'a', 'an',  \
+		'of', 'to', 'in', 'for', 'on', 'by', 'like', 'over', 'after']
+		newWords = []
+		words = value.split(' ')
+		newWords.push(words[0].capitalize)
+		words = words[1..-1]
+		words.each do |i|
+			if bads.include?(i) == true
+				newWords.push(i)
+			elsif bads.include?(i) == false
+				newWords.push(i.capitalize)
+			end
+		end
+		newWords = newWords.join(' ')
+		@title = newWords
+	end
 end
